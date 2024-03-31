22043cd5f58d7d2b45e717680214f7e9f4a2da4d
huanfeng <yeh.hua@northeastern.edu>
2024-03-31 14:54:44 -0700

    Initil commit

---
+++ app/src/main/java/get/elevate/com/MainActivity.kt
@@ -0,0 +1,11 @@
+package get.elevate.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
